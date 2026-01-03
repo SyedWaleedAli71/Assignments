f72b7ea37189e2d6dd9bcb570c93983e70fe6838
SyedWaleedAli71 <syedwaleed887@gmail.com>
2026-01-03 16:33:00 +0500

    first commit

---
+++ Assignment-12-main/app.js
@@ -0,0 +1,28 @@
+
+// Chapter No.1
+// Q.1
+// alert("Hey user Welcome to my website!")
+
+// Q.2
+// alert("Please enter a correct password!")
+// document.writeln("Please enter a correct password..!");
+
+// Q.3
+// document.writeln("Hey User.. <hr>");
+// document.writeln("How are You..!!<br>Welcome to my website");
+// alert("Hey User.. \nHow are You..!!");
+
+// Q.4
+// alert("Welcome to My web Page..!")
+// alert("Happy Coding..!")
+
+
+// Question 5
+// console.log("How are You..!!\nWelcome to my website");
+
+
+// Question 6
+// alert("Wellcome to E-commerce Web site");   
+
+// Question 7
+// Inside Of HTML File 
---
+++ Assignment-14-main/app.js
@@ -0,0 +1,39 @@
+// *******USER INPUT & CONDITIONAL STATEMENT****************//
+// Q.1
+
+// var city = prompt ("Enter your city name");
+// if (city === "Karachi")
+// {
+//     alert ("Welcome to the city of lights");
+// }
+
+
+// Q.2
+
+// var gender = prompt("Enter your gender");
+// if (gender === "Male"){
+//     alert("Good Morning Sir")
+// }
+// else if (gender === "Female"){
+//     alert("Good MOrning Ma'am");
+// };
+
+// Q.3
+// var color = prompt("Enter traffic signal color")
+
+// if (color === "Red"){
+//     alert("Must Stop");
+// }
+// else if (color === "Yellow"){
+//     alert("Ready to move");
+// }
+// else if (color === "Green"){
+//     alert("Move now");
+// };
+// Q.4
+// 
+// var fuel = prompt("Enter remaining fuel in litres:");
+
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// };
